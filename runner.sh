06dd542baae3f6894a0437cf7e1dfdf03b2ed08b
Zihao Zou <zihao@cigserver3.seas.wustl.edu>
2022-03-26 14:51:52 -0500

    no pt

--- runner.sh
+++
@@ -1,6 +0,0 @@
-python main_code/plTrain.py --conf_path main_code/config/unet_2.json
-python main_code/plTrain.py --conf_path main_code/config/unet_3.json
-python main_code/plTrain.py --conf_path main_code/config/unet_5.json
-python main_code/plTrain.py --conf_path main_code/config/spec_2.json
-python main_code/plTrain.py --conf_path main_code/config/spec_3.json
-python main_code/plTrain.py --conf_path main_code/config/spec_5.json
