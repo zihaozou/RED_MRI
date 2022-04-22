5246fe68c21393c382f938e2b8e57f1525b8f2dc
Zihao Zou <zihao@cigserver3.seas.wustl.edu>
2022-04-22 16:45:22 -0500

    vjp

--- main_code/model/jacob.py
+++ main_code/model/jacob.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch.autograd as autograd
-
+from torch.autograd.functional import vjp 
 
 class jacobinNet(nn.Module):
     """Unfold network models, i.e. (online) PnP/RED"""
@@ -14,3 +14,14 @@ class jacobinNet(nn.Module):
         J = autograd.functional.jacobian(
             f, x, create_graph=create_graph, strict=strict)
         return J
+
+class VJPNet(nn.Module):
+    def __init__(self, dnn: nn.Module):
+        super(jacobinNet, self).__init__()
+        self.dnn = dnn
+
+    def forward(self, x, create_graph=True, strict=True):
+        def f_(x): return self.dnn(x).mean([1, 2, 3]).sum()
+        def f(x): return autograd.functional.jacobian(f_, x, create_graph=create_graph, strict=strict)
+        vjVector=vjp(f,x,create_graph=create_graph, strict=strict)
+        return vjVector
